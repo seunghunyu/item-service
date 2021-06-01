517be44442735988c9247568c1a734868b5aa015
ysh <livefor1212@gmail.com>
2021-06-02 00:20:58 +0900

    ItemRepository Test code add commit

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -2,7 +2,9 @@ package hello.itemservice.domain.item;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Repository
@@ -16,4 +18,19 @@ public class ItemRepository {
         return item;
     }
 
+    public Item findById(Long id){
+        return store.get(id);
+    }
+    public List<Item> findAll(){
+        return new ArrayList<>(store.values());
+    }
+    public void update(Long itemId, Item updateParam){
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+    public void clearStore(){
+        store.clear();
+    }
 }
