3b6b80e179ba5b526107c803aaee3d1e606c03ef
ysh <livefor1212@gmail.com>
2021-06-01 23:54:44 +0900

    Item , ItemRepository add commit

---
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,19 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+    private static final Map<Long, Item> store = new HashMap<>(); //static
+    private static long sequence = 0L; //static
+
+    public Item save(Item item){
+        item.setId(++sequence);
+        store.put(item.getId(),item);
+        return item;
+    }
+
+}
