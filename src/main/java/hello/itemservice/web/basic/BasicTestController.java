276391fbad31d65b065ae165d50c9b87486f5eac
ysh <livefor1212@gmail.com>
2021-06-08 01:50:02 +0900

    sendForm add commit

---
+++ src/main/java/hello/itemservice/web/basic/BasicTestController.java
@@ -0,0 +1,33 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/sendForm")
+public class BasicTestController {
+    Logger log = LoggerFactory.getLogger(getClass());
+
+    @GetMapping
+    public String items(){
+        log.info("sendForm controller");
+        return "basic/sendForm";
+    }
+
+
+    @PostMapping("send")
+    String getDate(@PathVariable String itemName, Model model){
+        model.addAttribute("itemName",itemName);
+        return "basic/sendForm";
+    }
+}
