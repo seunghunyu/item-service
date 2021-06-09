bdea41b5fc6e982c6ebcf2df64eedca440c96e30
ysh <livefor1212@gmail.com>
2021-06-10 02:23:55 +0900

    redirect attributes add commit

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -3,10 +3,13 @@ package hello.itemservice.web.basic;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -16,7 +19,7 @@ import java.util.List;
 @RequiredArgsConstructor   // final이 붙은 필드를 가지고 @Autowired annotation이 붙은 생성자를 만들어주는 annotation
 public class BasicItemController {
     private final ItemRepository itemRepository;
-
+    Logger logger = LoggerFactory.getLogger(this.getClass());
     @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
@@ -33,6 +36,7 @@ public class BasicItemController {
 
     @GetMapping("/add")
     public String addForm() {
+        logger.info("addForm ~~~");
         return "basic/addForm";
     }
 
@@ -66,15 +70,28 @@ public class BasicItemController {
         itemRepository.save(item);
 //        model.addAttribute("item",item);  //자동추가,생략가능
 
-        return "basic/addForm";
+        return "basic/item";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV4(Item item) {
         itemRepository.save(item);
 //        model.addAttribute("item",item);  //자동추가,생략가능
 
-        return "basic/addForm";
+        return "basic/item";
+    }
+//    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items"+item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId",savedItem.getId());
+        redirectAttributes.addAttribute("status",true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
