898f2fc5f91f43c1ee86caa25f9b8207895222f5
ysh <livefor1212@gmail.com>
2021-09-25 01:54:08 +0900

    validation 수정관련 코드 적용 commit

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -5,17 +5,19 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message ="총합이 10000원이 넘도록 입력 해주세요.")
 public class Item {
 
     private Long id;
 
-    @NotBlank
+    @NotBlank(message = "공백X")
     private String itemName;
 
     @NotNull
