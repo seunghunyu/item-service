98f0839517d1d084b786f5c2e87323a8d2fb4eee
ysh <livefor1212@gmail.com>
2021-10-02 19:09:44 +0900

    스프링 interceptor 요청 로그 add commit

--- src/main/java/hello/itemservice/WebConfig.java
+++ src/main/java/hello/itemservice/WebConfig.java
@@ -1,16 +1,27 @@
 package hello.itemservice;
 
 import hello.itemservice.web.filter.LogFilter;
+import hello.itemservice.web.interceptor.LoginInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterRegistration;
 
 @Configuration
-public class WebConfig {
-    @Bean
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .order(1)
+                .addPathPatterns("/?*")
+                .excludePathPatterns("/css/**","/*.ico","/error");
+    }
+
+    //@Bean
     public FilterRegistrationBean logFilter(){
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
---
+++ src/main/java/hello/itemservice/web/interceptor/LoginInterceptor.java
@@ -0,0 +1,47 @@
+package hello.itemservice.web.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Slf4j
+public class LoginInterceptor implements HandlerInterceptor{
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+        request.setAttribute("logId",uuid);
+
+        //@RequestMapping : HandlerMethod
+        //정적 리소스 : ResourceHttpRequestHandler
+
+        if(handler instanceof HandlerMethod){
+            HandlerMethod hm = (HandlerMethod) handler;//호출할 컨트롤러 메소드의 모든정보가 호출되어있다.
+        }
+
+        log.info("REQUEST [{}][{}][{}]",uuid,requestURI, handler);
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("postHandle=[{}]", modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String requestURI = request.getRequestURI();
+        String logId = (String)request.getAttribute("logId");
+        log.info("REQUEST [{}][{}][{}]",logId,requestURI, handler);
+        if(ex != null){
+            log.error("afterCompletion error!! ", ex);
+        }
+    }
+}
