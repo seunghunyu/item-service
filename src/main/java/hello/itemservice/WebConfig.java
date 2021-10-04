85fc925a59005167642eed11c0c92bf619af81ea
seunghunyu <livefor1212@gmail.com>
2021-10-05 00:47:29 +0900

    ArgumentResolver 추가

--- src/main/java/hello/itemservice/WebConfig.java
+++ src/main/java/hello/itemservice/WebConfig.java
@@ -1,19 +1,27 @@
 package hello.itemservice;
 
+import hello.itemservice.web.argumentresolver.LoginMemberArgumentResolver;
 import hello.itemservice.web.filter.LogFilter;
 import hello.itemservice.web.interceptor.LoginCheckInterceptor;
 import hello.itemservice.web.interceptor.LoginInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterRegistration;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers){
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
--- src/main/java/hello/itemservice/web/HomeController.java
+++ src/main/java/hello/itemservice/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.itemservice.web;
 
 import hello.itemservice.domain.member.Member;
 import hello.itemservice.domain.member.MemberRepository;
+import hello.itemservice.web.argumentresolver.Login;
 import hello.itemservice.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -71,8 +72,19 @@ public class HomeController {
         model.addAttribute("member",loginMember);
         return "loginHome";
     }
-    @GetMapping("/")   //@SessionAttribue 스프링 제공 annotation -> 참고로 세션을 생성하지는 않음
+    //@GetMapping("/")   //@SessionAttribue 스프링 제공 annotation -> 참고로 세션을 생성하지는 않음
     public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER,required = false) Member loginMember, Model model){
+        //세션에 회원 데이터가 없으면 home
+        if(loginMember==null){
+            return "home";
+        }
+
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member",loginMember);
+        return "loginHome";
+    }
+    @GetMapping("/")   //@SessionAttribue 스프링 제공 annotation -> 참고로 세션을 생성하지는 않음
+    public String homeLoginV3SpringArgumentResolver(@Login Member loginMember, Model model){
         //세션에 회원 데이터가 없으면 home
         if(loginMember==null){
             return "home";
