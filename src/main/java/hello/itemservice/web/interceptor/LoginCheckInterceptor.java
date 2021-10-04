9662e6139a8041d7cfd45ea86d000ebd7835a51a
ysh <livefor1212@gmail.com>
2021-10-04 23:28:24 +0900

    LoginCheckInterceptor 추가

---
+++ src/main/java/hello/itemservice/web/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,8 @@
+package hello.itemservice.web.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+@Slf4j
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+}
