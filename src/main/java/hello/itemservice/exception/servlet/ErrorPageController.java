445f87987ab82f823e9c408b5ef0b0002ab75953
ysh <livefor1212@gmail.com>
2021-10-08 21:35:05 +0900

    ApiExceptionController 추가

--- src/main/java/hello/itemservice/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/itemservice/exception/servlet/ErrorPageController.java
@@ -1,11 +1,18 @@
 package hello.itemservice.exception.servlet;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @Controller
@@ -29,7 +36,16 @@ public class ErrorPageController {
         log.info("errorPage 500");
         return "error-page/500";
     }
+    @RequestMapping(value = "/error-page/500",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String,Object>> errorPage500Api(HttpServletRequest request, HttpServletResponse response){
+        Map<String, Object> result = new HashMap<>();
+        Exception ex = (Exception)request.getAttribute(ERROR_EXCEPTION);
+        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message", ex.getMessage());
+        Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
+        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
+    }
     private void printErrorInfo(HttpServletRequest request){
         log.info("ERROR_EXCEPTION: {}", request.getAttribute(ERROR_EXCEPTION));
         log.info("ERROR_EXCEPTION_TYPE: {}", request.getAttribute(ERROR_EXCEPTION_TYPE));
