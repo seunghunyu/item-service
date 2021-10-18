2fa9f0ff79ec0e4812e42c9fc0d95223b67226c8
ysh <livefor1212@gmail.com>
2021-10-19 00:47:19 +0900

    @ControllerAdvice 추가

--- src/main/java/hello/itemservice/exception/exhandler/advice/ExControllerAdvice.java
+++ src/main/java/hello/itemservice/exception/exhandler/advice/ExControllerAdvice.java
@@ -1,5 +1,6 @@
-package hello.itemservice.exception.api;
+package hello.itemservice.exception.exhandler.advice;
 
+import hello.itemservice.exception.api.ApiExceptionV2Controller;
 import hello.itemservice.exception.exception.UserException;
 import hello.itemservice.exception.exhandler.ErrorResult;
 import lombok.AllArgsConstructor;
@@ -10,9 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@RestController
-public class ApiExceptionV2Controller {
-    /*
+@RestControllerAdvice(basePackages = "hello.exception.api")
+public class ExControllerAdvice {
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
     public ErrorResult illegalExHandler(IllegalArgumentException e){
@@ -25,27 +26,13 @@ public class ApiExceptionV2Controller {
         log.error("[exceptionHandler ex",e);
         ErrorResult errorResult = new ErrorResult("USER-EX",e.getMessage());
         return new ResponseEntity(errorResult, HttpStatus.BAD_REQUEST);
-    }*/
-
-    @GetMapping("/api2/members/{id}")
-    public MemberDto getMember(@PathVariable("id") String id){
-
-        if(id.equals("ex")){
-            throw new RuntimeException("잘못된 사용자");
-        }
-        if(id.equals("bad")){
-            throw new IllegalArgumentException("잘못된 입력 값");
-        }
-        if(id.equals("user-ex")){
-            throw new UserException("사용자 오류");
-        }
-        return new MemberDto(id, "hello "+ id);
     }
 
-    @Data
-    @AllArgsConstructor
-    static class MemberDto{
-        private String memberId;
-        private String name;
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e){
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("EX","내부오류");
     }
+
 }
