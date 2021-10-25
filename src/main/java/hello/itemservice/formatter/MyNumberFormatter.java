49f2f8464db627b766e974e4a57c187d584d7f90
ysh <livefor1212@gmail.com>
2021-10-26 00:03:08 +0900

    MyNumberFormatterTest코드 추가

--- src/main/java/hello/itemservice/formatter/MyNumberFormatter.java
+++ src/main/java/hello/itemservice/formatter/MyNumberFormatter.java
@@ -3,6 +3,7 @@ package hello.itemservice.formatter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.Formatter;
 
+import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
@@ -11,11 +12,15 @@ public class MyNumberFormatter implements Formatter<Number> {
 
     @Override
     public Number parse(String text, Locale locale) throws ParseException {
-        return null;
+        log.info("text={},locale={}", text,locale);
+        //"1,000" -> 1000
+        NumberFormat format = NumberFormat.getInstance(locale);
+        return format.parse(text);
     }
 
     @Override
     public String print(Number object, Locale locale) {
-        return null;
+        log.info("object={},locale={}", object,locale);
+        return NumberFormat.getInstance(locale).format(object);
     }
 }
