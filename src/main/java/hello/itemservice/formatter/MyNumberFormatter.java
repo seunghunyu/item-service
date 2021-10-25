5fb4aa8bd88f08b53e8f0a8400a2ee33ab1563c3
ysh <livefor1212@gmail.com>
2021-10-25 23:52:33 +0900

    MyNumberFormatter 추가

---
+++ src/main/java/hello/itemservice/formatter/MyNumberFormatter.java
@@ -0,0 +1,21 @@
+package hello.itemservice.formatter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.Formatter;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+@Slf4j
+public class MyNumberFormatter implements Formatter<Number> {
+
+    @Override
+    public Number parse(String text, Locale locale) throws ParseException {
+        return null;
+    }
+
+    @Override
+    public String print(Number object, Locale locale) {
+        return null;
+    }
+}
