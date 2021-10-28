350d336c5a79a4b893a4aa9de643036965b47220
ysh <livefor1212@gmail.com>
2021-10-28 21:58:40 +0900

    ServletUploadControllerV1 add

---
+++ src/main/java/hello/itemservice/upload/ServletUploadControllerV1.java
@@ -0,0 +1,12 @@
+package hello.itemservice.upload;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/servlet/v1")
+public class ServletUploadControllerV1 {
+
+}
