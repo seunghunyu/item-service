5190e5c42ee7decd69a81c1727cbb26fdbd535d7
ysh <livefor1212@gmail.com>
2023-05-29 16:35:11 +0900

    h2 임베디드 모드 추가

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,12 +2,16 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
+@Slf4j
 //@Import(MemoryConfig.class)//[2023.04.25]
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
@@ -24,5 +28,19 @@ public class ItemServiceApplication {
 	@Profile("local")
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
+	}
+
+	@Bean
+	@Profile("test")
+	public DataSource dataSource(){
+		log.info("메모리 데이터 베이스 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		//jdbc:h2:mem:db : h2를 jvm 에 올려놓고 쓸수 있는 임베디드 모드로 사용
+		//DB_CLOSE_DELAY=-1 임베디드 모드에서는 데이터베이스 커넥션 연결이 모두 끊어지면 데이터 베이스도 종료되는데 그것을 방지하는 설정
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
 	}
 }
