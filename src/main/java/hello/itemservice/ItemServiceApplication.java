89dfd62e304f94604f901b41b17f474cffdac161
ysh <livefor1212@gmail.com>
2023-06-04 16:23:31 +0900

    MyBatis 기본 설정 및 쿼리 xml 추가

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -30,17 +30,17 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
 
-	@Bean
-	@Profile("test")
-	public DataSource dataSource(){
-		log.info("메모리 데이터 베이스 초기화");
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.h2.Driver");
-		//jdbc:h2:mem:db : h2를 jvm 에 올려놓고 쓸수 있는 임베디드 모드로 사용
-		//DB_CLOSE_DELAY=-1 임베디드 모드에서는 데이터베이스 커넥션 연결이 모두 끊어지면 데이터 베이스도 종료되는데 그것을 방지하는 설정
-		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-		dataSource.setUsername("sa");
-		dataSource.setPassword("");
-		return dataSource;
-	}
+//	@Bean
+//	@Profile("test")
+//	public DataSource dataSource(){
+//		log.info("메모리 데이터 베이스 초기화");
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("org.h2.Driver");
+//		//jdbc:h2:mem:db : h2를 jvm 에 올려놓고 쓸수 있는 임베디드 모드로 사용
+//		//DB_CLOSE_DELAY=-1 임베디드 모드에서는 데이터베이스 커넥션 연결이 모두 끊어지면 데이터 베이스도 종료되는데 그것을 방지하는 설정
+//		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+//		dataSource.setUsername("sa");
+//		dataSource.setPassword("");
+//		return dataSource;
+//	}
 }
