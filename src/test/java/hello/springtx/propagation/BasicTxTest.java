53e96c67aead0c460bf09e0d37666664883f97ae
ysh <livefor1212@gmail.com>
2023-08-08 01:21:47 +0900

    스프링 트랜잭션 전파 7 - REQUIRES_NEW

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
@@ -125,6 +126,26 @@ public class BasicTxTest {
 
         log.info("외부 트랜잭션 커밋");
         txManager.commit(outer);
+        Assertions.assertThatThrownBy(()-> txManager.commit(outer))
+                    .isInstanceOf(UnexpectedException.class);
+
+    }
+    @Test
+    void inner_rollback_requires_new(){
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction = {}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        DefaultTransactionAttribute definition = new DefaultTransactionAttribute();
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        TransactionStatus inner = txManager.getTransaction(definition);
+        log.info("inner.isNewTransaction = {}", inner.isNewTransaction()); // true
+        log.info("내부 트랜잭션 롤백");
+        txManager.rollback(inner); // 롤백
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer); // 커밋
+
         Assertions.assertThatThrownBy(()-> txManager.commit(outer))
                     .isInstanceOf(UnexpectedException.class);
 
